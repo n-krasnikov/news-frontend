72745184a754449e1c32dd69b9876320b186592d
Nitita Krasnikov <n.krasnikov@tg.dunice.net>
2023-07-24 11:41:56 +0300

    sign in/up locales->const

--- src/components/Header/constants.ts
+++ src/components/Header/constants.ts
@@ -1,7 +1,5 @@
-import { 
-  SIGN_IN, 
-  SIGN_UP, 
-} from '../../locales.json'
+const SIGN_IN: string = 'Sign In';
+const SIGN_UP: string = 'Sign Up';
 
 export const BUTTONS = [SIGN_IN, SIGN_UP];
 export const TOKEN = 'token';
