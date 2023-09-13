06903fd7dfd7dc4fab041213e04f83fd85d16b4d
Krasnilov Nikita <n.krasnikov@tg.dunice.net>
2023-09-13 10:34:08 +0300

    redesigned user-card

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,22 +1,14 @@
 import { type FC } from 'react';
 import { useDispatch } from 'react-redux';
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import ProfileEditIcon from '@mui/icons-material/AppRegistration';
 
 import { toggleModal } from '../../redux/actions/modal';
 import { ModalType } from '../../vite-env';
 import { IUserCard } from './UserCard.props';
 import { ADD_POST_TYPE, EDIT_PROFILE_TYPE } from './constants';
-import { 
-  EDIT_PROFILE,
-  CREATE_POST,
-  AVATAR_ALT,
-} from '../../locales.json';
 
 import defaultImage from '../../assets/react.svg';
 import './UserCard.css';
@@ -34,37 +26,29 @@ export const UserCard: FC<IUserCard> = ({
     dispatch(toggleModal({ status: true, type}));
   };
 
-  return (
-    <Card className='user-card'>
-      <CardMedia
-        className='avatar'
-        component='img'
-        image={avatar || defaultImage}
-        alt={AVATAR_ALT}
-      />
-      <CardContent className='card-content'>
-        <Typography 
-          gutterBottom 
-          variant='h5' 
-          component='div' 
-          className='post-title'
-        >
-          {username}
-        </Typography>
-        <Typography variant='body2' className='post-desc'>
-          {email}
-        </Typography>
-        <Typography variant='body2' className='post-desc'>
-            Author of {postsCount} posts
-        </Typography>
-        {isOwnerPage && (
-          <CardActions className='card-footer'>
-            <Button onClick={()=> openModal(EDIT_PROFILE_TYPE)}>{EDIT_PROFILE}</Button>
-            <Button onClick={() => openModal(ADD_POST_TYPE)}>{CREATE_POST}</Button>
-          </CardActions>
-        )}
-      </CardContent>
+  const postsInfo = postsCount
+    ? `Author of ${postsCount} posts`
+    : 'No posts';
 
-    </Card>
+  return (
+    <>
+    <div className='user-card'>
+      <Avatar 
+        src={avatar || defaultImage}
+        className='user-card-avatar'
+      />
+      <div className='user-card-info'>
+        <h3>{username}</h3>
+        <span>{postsInfo}</span>
+        <span>{email}</span>
+      </div>
+      {isOwnerPage && 
+        (<div className='user-card-actions'>
+          <PostAddIcon onClick={()=> openModal(EDIT_PROFILE_TYPE)}/>
+          <ProfileEditIcon onClick={()=> openModal(ADD_POST_TYPE)}/>
+        </div>)
+      }
+    </div>
+    </>
   );
 };
--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,62 +1,11 @@
-import { useEffect, type FC } from 'react';
+import { type FC } from 'react';
 
 import './TestPage.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { postsRequest } from '../../redux/actions/posts';
-import { IPost, IPostState } from '../../vite-env';
-import { RootState } from '../../redux/reducers/rootReducer';
-import { NoteCard } from '../../components/NoteCard';
-
-import longImg from '../../assets/image.webp';
-import shortImg from '../../assets/img.png';
 
 export const TestPage: FC = () => {
-  const { posts, isLoading, error }: IPostState = useSelector((store: RootState) => store.posts);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(postsRequest());
-  }, []);
-
-  // console.log(isLoading, posts, error);
-
-  const fakeData: IPost = {
-    author: '_admin',
-    author_id: 2,
-    id: 6,
-    image: undefined,
-    tags: 'tag_1,tag_2,tag3',
-    text: 'some large text',
-    title: 'Title'
-  };
-
-  const fakeDataLongImg: IPost = {
-    author: '_admin',
-    author_id: 2,
-    id: 6,
-    image: longImg,
-    tags: 'tag_1,tag_2,tag3',
-    text: 'some large text',
-    title: 'Title'
-  };
-
-  const fakeDataShortImg: IPost = {
-    author: '_admin',
-    author_id: 2,
-    id: 6,
-    image: shortImg,
-    tags: 'tag_1,tag_2,tag_3,tag_4,tag_5,tag_6,tag_7,tag_8,tag_9,tag_0',
-    text: 'some large text',
-    title: 'Title'
-  };
-
   return(
     <div className='container'>
-      {/* <h1>{fakeData.author}</h1> */}
-      <NoteCard {...fakeDataShortImg} />
-      <NoteCard {...fakeDataLongImg} />
-      <NoteCard {...fakeData} />
-      <NoteCard {...fakeData} />
+      
     </div>
   );
 };
